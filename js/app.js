2fefacd34c8404e3dbbe7a621c9258f9fefc7aed
Waduzzaman <waduzzaman@gmail.com>
2022-09-02 22:15:44 -0400

    blog.html page created

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+const loadCategory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+
+
+loadCategory();
