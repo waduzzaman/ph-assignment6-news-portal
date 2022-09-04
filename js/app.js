b741746bde73a760403f9826660495bbb898c71d
Waduzzaman <waduzzaman@gmail.com>
2022-09-04 00:55:06 -0400

    art news seciton added

--- js/app.js
+++ js/app.js
@@ -355,6 +355,60 @@ const displayCultureNews = news => {
     });
 }
 
-loadCultureNews()
+// loadCultureNews()
 
+// Arts News Section 
+
+const loadArtsNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/07`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayArtsNews(data.data);
+
+}
+
+
+const displayArtsNews = news => {
+    const artsNewsDiv = document.getElementById('artsNewsContainer');
+    news.forEach(category => {
+        console.log(category);
+        const newsDetailsDiv = document.createElement('div');
+        newsDetailsDiv.classList.add('col');
+        newsDetailsDiv.innerHTML = `
+        <div class="row g-1 mb-3">
+
+                        <div class="col-md-4">
+                            <img src="${category.thumbnail_url}..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="}card-title">${category.title}</h5>
+                                <p class="card-text">${category.details}</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <div class = "d-flex justify-content-evenly">
+                            <div class="d-flex just">
+                             <img class="rounded-circle w-25" src = "${category.author.img} 
+                             alt = "" >
+                             <div>
+                             <h6>${category.author.name}</h6>
+                            <p>${category.author.published_date}</p>
+                            </div>                            
+                            </div>
+                            <div><h6><i class="fa-regular fa-eye"></i>${category.total_view}</h6></div>                                    
+                          
+                            </div>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                      
+        `;
+        artsNewsDiv.appendChild(newsDetailsDiv);
+
+
+    });
+}
+
+// loadArtsNews()
 
